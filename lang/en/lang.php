d336d85e71b530e5ce616ba22b484576f312305f
Ronaldo Ribeiro de Sousa <ribbsousa@gmail.com>
2017-03-03 11:58:36 -0300

    Update lang.php

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -3,6 +3,8 @@
     return [
         'backend' => [
             'featuredvideo' => 'Featured Video',
-            'description' => 'Video Iframe content'
+            'description' => 'Video Iframe content',
+            'featuredimages' => 'Featured Video Images',
+            'featuredimages_description' => 'Add featured image to your video'
         ]
     ];
